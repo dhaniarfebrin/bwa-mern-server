26a7c80e255bc7a43ef461d79c0a05967a857825
dhaniarfebrinm <dhaniarfebrinwahyudi@gmail.com>
2022-10-03 10:35:36 +0700

    adding category feature

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,4 +1,5 @@
 const Voucher = require('../voucher/model')
+const Category = require('../category/model')
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -28,6 +29,15 @@ module.exports = {
         res.status(200).json({ data: voucher })
     } catch (err) {
         res.status(500).json({message: err.message || `Internal server error`})
+    }
+  },
+  category: async (req, res) => {
+    try {
+      const category = await Category.find()
+
+      res.status(200).json({ data: category })
+    } catch (err) {
+      res.status(500).json({message: err.message || `Internal server error`})
     }
   }
 }
--- app/player/router.js
+++ app/player/router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { landingPage, detailPage } = require("./controller");
+const { landingPage, detailPage, category } = require("./controller");
 
 // ruters of pages
 router.get('/landingpage', landingPage)
+router.get('/category', category)
 router.get('/:id/detail', detailPage)
 
 module.exports = router
